c55792f6d2f99e18f2c08509f79805b27a8f32db
MikeJDev <ukmjanes@gmail.com>
2019-02-19 17:26:44 -0600

    changed port number to 3003

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require ('body-parser');
 const app = express()
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3003;
 const db = require('../database/dbConnection.js')
 
 app.use("/", express.static("dist"));
