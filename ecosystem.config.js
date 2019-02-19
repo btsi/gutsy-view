48b18e115b522b372c00f3f0d82d3cfa7f1398eb
MikeJDev <ukmjanes@gmail.com>
2019-02-19 13:42:06 -0600

    updated pm2 script src

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [{
     name: 'gutsy-project-view',
-    script: './index.js'
+    script: './src/index.js'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
       host: 'ec2-18-216-74-131.us-east-2.compute.amazonaws.com',
-      key: '/Users/mikejanes/.ssh/Gutsy2.pem',
+      key: '~/.ssh/Gutsy2.pem',
       ref: 'origin/master',
       repo: 'https://github.com/MikeJDev/gutsy-view.git',
       path: '/home/ubuntu/server',
