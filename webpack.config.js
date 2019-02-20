d213de27c0d17771121bac1e0995f1719e168de9
MikeJDev <ukmjanes@gmail.com>
2019-02-20 12:04:25 -0600

    added absolute path to include:src

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        include: './src',
+        include: '/Users/mikejanes/Desktop/gutsy-view/src',
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
